1c68f2fc03633de16f356008ac0cc0d92761dac8
Tomas Guajardo <tomas.guajardo02@utrgv.edu>
2025-10-01 22:13:39 +0000

    class work2

--- class_work2/src/main.rs
+++ class_work2/src/main.rs
@@ -20,49 +20,11 @@ impl Student {
     }
 }
 
-struct ParkingSystem {
-    big: i32,
-    medium: i32,
-    small: i32,
-}
-
-impl ParkingSystem {
-    fn new(big: i32, medium: i32, small: i32) -> Self {
-        ParkingSystem { big, medium, small }
-    }
-
-    fn add_car(&mut self, car_type: i32) -> bool {
-        match car_type {
-            1 if self.big > 0 => {
-                self.big -= 1;
-                true
-            }
-            2 if self.medium > 0 => {
-                self.medium -= 1;
-                true
-            }
-            3 if self.small > 0 => {
-                self.small -= 1;
-                true
-            }
-            _ => false,
-        }
-    }
-}
-
 fn main() {
     // ===== Assignment 1: Student Struct =====
     let mut student1 = Student::new("Tomas Guajardo", "Computer Engineering");
     println!("Student: {}, Major: {}", student1.name, student1.get_major());
 
-    student1.set_major("Computer engineering");
+    student1.set_major("Computer Engineering");
     println!("Updated Major: {}", student1.get_major());
-
-    // ===== Assignment 2: Parking System =====
-    let mut ps = ParkingSystem::new(1, 1, 0); // 1 big, 1 medium, 0 small
-
-    println!("Add big car: {}", ps.add_car(1));   // true
-    println!("Add medium car: {}", ps.add_car(2)); // true
-    println!("Add small car: {}", ps.add_car(3));  // false
-    println!("Add big car again: {}", ps.add_car(1)); // false
 }
